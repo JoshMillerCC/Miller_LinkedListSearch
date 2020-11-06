6c659ec54ca2b18cd41f825a84ad563a474ec841
joshua miller <joshua.miller@student.centralia.edu>
2020-11-05 22:48:41 -0800

    added .txt to file name in the string path

--- Miller_LinkedListSearch/LoadFile.cs
+++ Miller_LinkedListSearch/LoadFile.cs
@@ -22,7 +22,7 @@ namespace Miller_LinkedListSearch
             LinkedList list = new LinkedList();
 
             string fileDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
-            string path = Path.Combine(fileDirectory, "yob2019");
+            string path = Path.Combine(fileDirectory, "yob2019.txt");
 
             StreamReader file = new StreamReader(path);
             while((fileData = file.ReadLine()) != null)
